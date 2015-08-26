4b0ea35e55a54693dc0c79d9a33c03d674322496
Matthew Chastain <matthew.chastain@benefitfocus.com>
2015-08-26 11:52:24 -0400

    Removed dist

---
+++ dist/templates/welcome-page.js
@@ -0,0 +1,1 @@
+(function(dust){dust.register("welcome-page.dust",body_0);function body_0(chk,ctx){return chk.w("<div class=\"welcome-text\">text</div><div class=\"welcome-nav\">nav</div>");}body_0.__dustBody=!0;return body_0}(dust));
