7d5bf2f78c8063c98d7b94f299819c690c77738c
Matthew Chastain <matthew.chastain@benefitfocus.com>
2015-09-10 17:05:30 -0400

    minor tweak

--- scripts/models/welcome.js
+++ scripts/models/welcome.js
@@ -4,6 +4,6 @@ define(function() {
 			main: 'Welcome Back!',
 			sublink: 'Click below to watch an important video about this website!',
 			buttonText: 'Watch Now!',
-			subText: 'Welcome! This year, we are participating in regular classroom structure and also exploring "blended learning." This means we will be using tablets and phones in class to watch videos and answer questions, create, explore student-led learning, and engage in lots of fun! To find out more about blended learning, you can click <a href="https://www.khanacademy.org/partner-content/ssf-cci/sscc-intro-blended-learning">here</a> to visit the Khan Academy for in-depth information - or, you can <a href="/#contact">contact Mrs. C</a> if you have any questions.'
+			subText: 'Welcome! This year, we are participating in regular classroom structure and also exploring "blended learning." This means we will be using tablets and phones in class to watch videos and answer questions, create, explore student-led learning, and engage in lots of fun! To find out more about blended learning, you can click <a href="https://www.khanacademy.org/partner-content/ssf-cci/sscc-intro-blended-learning" target="_blank">here</a> to visit the Khan Academy for in-depth information - or, you can <a href="/#contact">contact Mrs. C</a> if you have any questions.'
 		}
 	});
